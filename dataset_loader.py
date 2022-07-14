35676e676f1953c6973a6da1035e73ed6cc42074
KingChad127 <abhinav.chadaga@utexas.edu>
2022-07-13 21:58:14 -0500

    create CLIPSet and visualization nb

---
+++ dataset_loader.py
@@ -0,0 +1,64 @@
+import os
+import pickle
+from typing import Tuple
+from termcolor import cprint
+import torch
+import pytorch_lightning as pl
+from lidar_helper import get_stack
+import numpy as np
+
+from torch.utils.data import Dataset, DataLoader
+
+
+class CLIPSet(Dataset):
+    def __init__(self, pickle_file_path: str, delay=30, visualize=False) -> None:
+        """create a CLIPSet object
+
+        capture data from the pickle file
+        get path to the bev lidar images but do not load into memory
+
+        args:
+            pickle_file_path: path to the processed pickle file
+            delay_frame: number of frames to skip from the start,
+                automatically set to 30 if argument is less than 
+        """
+
+        # check if the pickle file path exists
+        # check if the pickle file is processed using file suffix
+        if not os.path.exists(pickle_file_path.replace('_data.pkl', '_final.pkl')):
+            raise Exception(
+                "Pickle file does not exist. Please process the pickle file first..")
+
+        # load pickle file into data attribute
+        cprint('Pickle file exists. Loading from pickle file')
+        self.data = pickle.load(
+            open(pickle_file_path.replace('_data.pkl', '_final.pkl'), 'rb'))
+
+        # get lidar image information
+        self.lidar_dir: str = pickle_file_path[:-4]
+        self.lidar_img_paths = os.listdir(self.lidar_dir)
+
+        # set delay frame to be at least 30
+        self.delay = delay if delay > 30 else 30
+
+        self.visualize = visualize
+
+        cprint('Delay frame is : ' + str(self.delay),
+               'yellow', attrs=['bold'])
+
+    def __len__(self) -> int:
+        return len(self.lidar_img_paths)
+
+    def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray]:
+        # offset index
+        index = self.delay + index
+
+        lidar_stack = get_stack(odom=self.data['odom'],
+                                        lidar_path=self.lidar_dir,
+                                        i=index)
+
+        if not self.visualize:
+            lidar_stack = lidar_stack[0]
+
+        future_joy_data = self.data['future_joystick'][index]
+        return lidar_stack, future_joy_data
---
+++ lidar_helper.py
@@ -0,0 +1,70 @@
+import os
+import numpy as np
+import cv2
+from scipy.spatial.transform import Rotation as R
+from PIL import Image
+
+
+def affineinverse(M) -> np.ndarray:
+    tmp = np.hstack((M[:2, :2].T, -M[:2, :2].T @ M[:2, 2].reshape((2, 1))))
+    return np.vstack((tmp, np.array([0, 0, 1])))
+
+
+def get_affine_matrix_quat(x, y, quaternion) -> np.ndarray:
+    theta = R.from_quat(quaternion).as_euler('XYZ')[2]
+    return np.array([[np.cos(theta), -np.sin(theta), x],
+                     [np.sin(theta), np.cos(theta), y],
+                     [0, 0, 1]])
+
+
+def get_stack(odom: dict, lidar_path: str, i: int):
+    lidar_stack = [np.array(Image.open(os.path.join(
+        lidar_path, f'{x}.png'))) for x in range(i - 20 + 1, i + 1)]
+    odom_stack = odom[i - 20 + 1: i + 1]
+    lidar_stack = [lidar_stack[i] for i in [0, 5, 10, 15, 19]]
+    odom_stack = [odom_stack[i]
+                           for i in [0, 5, 10, 15, 19]]
+
+    # for visualization purposes
+    img_file_names = [f'{x}.png' for x in range(i - 20 + 1, i + 1)]
+    img_file_names = [img_file_names[i]
+                      for i in [0, 5, 10, 15, 19]]
+
+    # rotate previous frames to current frame
+    last_frame = odom_stack[-1]
+    T_odom_5 = get_affine_matrix_quat(
+        last_frame[0], last_frame[1], last_frame[2])
+    T_odom_4 = get_affine_matrix_quat(odom_stack[-2][0],
+                                      odom_stack[-2][1],
+                                      odom_stack[-2][2])
+    T_4_5 = affineinverse(T_odom_4) @ T_odom_5
+    T_odom_3 = get_affine_matrix_quat(odom_stack[-3][0],
+                                      odom_stack[-3][1],
+                                      odom_stack[-3][2])
+    T_3_5 = affineinverse(T_odom_3) @ T_odom_5
+    T_odom_2 = get_affine_matrix_quat(odom_stack[-4][0],
+                                      odom_stack[-4][1],
+                                      odom_stack[-4][2])
+    T_2_5 = affineinverse(T_odom_2) @ T_odom_5
+    T_odom_1 = get_affine_matrix_quat(odom_stack[-5][0],
+                                      odom_stack[-5][1],
+                                      odom_stack[-5][2])
+    T_1_5 = affineinverse(T_odom_1) @ T_odom_5
+    # now do the rotations
+    T_1_5[:, -1] *= -20
+    T_2_5[:, -1] *= -20
+    T_3_5[:, -1] *= -20
+    T_4_5[:, -1] *= -20
+    lidar_stack[0] = cv2.warpAffine(
+        lidar_stack[0], T_1_5[:2, :], (401, 401))
+    lidar_stack[1] = cv2.warpAffine(
+        lidar_stack[1], T_2_5[:2, :], (401, 401))
+    lidar_stack[2] = cv2.warpAffine(
+        lidar_stack[2], T_3_5[:2, :], (401, 401))
+    lidar_stack[3] = cv2.warpAffine(
+        lidar_stack[3], T_4_5[:2, :], (401, 401))
+
+    # combine the 5 single-channel images into a single image of 5 channels
+    lidar_stack = np.asarray(lidar_stack).astype(np.float32)
+    lidar_stack = lidar_stack / 255.0  # normalize the image
+    return lidar_stack, img_file_names
