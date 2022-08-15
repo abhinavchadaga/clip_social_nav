ec9377471cb1f09088182c1d283c9674b79be9e1
abhinavchadaga <abhinav.chadaga@utexas.edu>
2022-08-15 13:33:09 -0500

    complete process_rosbag.py

--- src/rosbag_parser/scripts/process_rosbag.py
+++ src/rosbag_parser/scripts/process_rosbag.py
@@ -1,46 +1,69 @@
-#!usr/bin/env python
-import argparse
+#!/usr/bin/env python
 import os
+import math
+import pathlib
+from re import S
+import subprocess
+import pickle
 
 import numpy as np
-import pickle
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseStamped, TransformStamped, Twist
-from sensor_msgs.msg import PointCloud2, Joy
 import rospy
-import message_filters
-import os
 import rosbag
+import message_filters
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import PointCloud2, Joy
 import yaml
-import math
-import subprocess
-from scipy.spatial.transform import Rotation as R
 from termcolor import cprint
 from tqdm.auto import tqdm
 
-data = {'lidar': [1, 2, 3], 'odom': [1, 2, 3],
-        'joystick': [1, 2, 3], 'goals': [1, 2, 3]}
+# data to save from rosbag
+data = {
+    'lidar_msgs_sync': [],
+    'odom_msgs_sync': [],
+    'joystick_msgs_sync': []
+}
 
 
-def callback(lidar_msg, odom_msg, joystick_msg):
-    pass
+def joystickValue(x, scale, kDeadZone=0.02) -> float:
+    if kDeadZone != 0.0 and abs(x) < kDeadZone:
+        return 0.0
+    return ((x - np.sign(x) * kDeadZone) / (1.0 - kDeadZone) * scale)
 
 
-def save_data(save_path, rosbag_path):
-    rosbag_name = rosbag_path.split('/')[-1]
-    save_path = os.path.join(save_path, rosbag_name)
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+def convert_joystick_msg(joystick_msg, robot_config) -> list:
+    joy_axes = joystick_msg.axes
+    linear_x = joystickValue(
+        joy_axes[robot_config['kXAxis']], -robot_config['kMaxLinearSpeed'])
+    linear_y = joystickValue(
+        joy_axes[robot_config['kYAxis']], -robot_config['kMaxLinearSpeed'])
+    angular_z = joystickValue(
+        joy_axes[robot_config['kRAxis']], -np.deg2rad(90.0), kDeadZone=0.0)
 
-    for k in data.keys():
-        pickle.dump(data[k], open(os.path.join(save_path, k)))
+    return [linear_x, linear_y, angular_z]
+
+
+def callback(lidar, odom, joystick):
+    # append synced lidar points
+    data['lidar_msgs_sync'].append(lidar)
+
+    # get xyz, quaternion from odom msg
+    x = odom.pose.pose.position.x
+    y = odom.pose.pose.position.y
+    orientation = odom.pose.pose.orientation
+    quaternion = [orientation.x, orientation.y, orientation.z, orientation.w]
+
+    # save odom msg
+    data['odom_msgs_sync'].append([x, y, quaternion])
+
+    # save joystick msg
+    data['joystick_msgs_sync'].append(joystick)
 
 
 def main():
-    rospy.init_node('process_rosbag', anonymous=True)
+    rospy.init_node('rosbag_parser', anonymous=True)
     rosbag_path = rospy.get_param('rosbag_path')
     save_data_path = rospy.get_param('save_data_path')
-    robot_name = rospy.get_param('robot_name')
+    robot_config_path = rospy.get_param('robot_config_path')
     # check if path to rosbag exists
     if not os.path.exists(rosbag_path):
         raise Exception('invalid rosbag path')
@@ -55,52 +78,76 @@ def main():
         [lidar_sub, odom_sub, joystick_sub], 100, 0.5, allow_headerless=True)
     time_synchronizer.registerCallback(callback)
 
-    # load config for robot
-    # find root of the ros node and config file path
-    package_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    config_file_path = os.path.join(
-        package_root, 'robot_config/'+str(robot_name)+'.yaml')
-    config = yaml.safe_load(config_file_path)
-
     # load rosbag
-    rosbag = rosbag.Bag(rosbag_path)
+    bag = rosbag.Bag(rosbag_path)
+    # load robot config yaml
+    robot_config_path = yaml.safe_load(open(robot_config_path))
+    cprint('loaded robot config: ', 'white', attrs=['bold'])
+    print(robot_config_path)
+
+    # create data directory
+    pathlib.Path(save_data_path).mkdir(parents=True, exist_ok=True)
+    # use rosbag name to create path to pkl output file
+    path_to_pkl = os.path.join(
+        save_data_path,
+        rosbag_path.split('/')[-1].replace('.bag', '.pkl'))
 
     # pre-record all odom messages
     # these are not time synchronized with the lidar frames
-    cprint('reading all odom messages...', 'green')
-    odom_msgs = {}
-    for topic, msg, t in tqdm(rosbag.read_messages(topics=['/odom'])):
-        if len(odom_msgs) == 0:
-            odom_msgs[0.0] = msg
+    # print('\n')
+    odom_msgs, odom_ts = [], []
+    cprint('Reading all odom messages...', 'green', attrs=['bold'])
+    for _, msg, t in tqdm(bag.read_messages(topics=['/odom'])):
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        orientation = msg.pose.pose.orientation
+        quaternion = [orientation.x, orientation.y,
+                      orientation.z, orientation.w]
+        odom_msgs.append([x, y, quaternion])
+        if len(odom_ts) == 0:
+            odom_ts.append(0.0)
         else:
-            odom_msgs[t.to_sec()] = msg
-    cprint('Done reading odom messages from the rosbag',
-           color='green')
+            odom_ts.append(t.to_sec())
+
+    data['odom_msgs'] = odom_msgs
+    data['odom_ts'] = odom_ts
+
+    cprint('Done reading odom messages from the rosbag\n',
+           color='green',
+           attrs=['bold'])
 
     # pre-record all the joystick messages
     # also not time synchronized with the lidar frames
     cprint(
         'Now reading all the joystick messages and timestamps from the rosbag',
-        'green')
-    joystick_msgs = {}
-    for _, msg, t in tqdm(rosbag.read_messages(topics=['/joystick'])):
-        if len(joystick_msgs) == 0:
-            joystick_msgs[0.0] = msg
+        'green',
+        attrs=['bold'])
+    joystick_msgs, joystick_ts = [], []
+    for _, msg, t in tqdm(bag.read_messages(topics=['/joystick'])):
+        joystick_msgs.append(convert_joystick_msg(msg, robot_config_path))
+        if len(joystick_ts) == 0:
+            joystick_ts.append(0.0)
         else:
-            joystick_msgs[t.to_sec()] = msg
-    cprint('Done reading joystick messages and timestamps',
+            joystick_ts.append(t.to_sec())
+
+    data['joystick_msgs'] = joystick_msgs
+    data['joystick_ts'] = joystick_ts
+
+    cprint('Done reading joystick messages and timestamps\n',
            color='green',
            attrs=['bold'])
 
-    info_dict = yaml.safe_load(rosbag._get_yaml_info())
+    # load play information from rosbag
+    info_dict = yaml.safe_load(bag._get_yaml_info())
     duration = info_dict['end'] - info_dict['start']
-    cprint('rosbag_length: ', duration)
+    cprint(f'rosbag_length: {duration:.2f} s', 'green', attrs=['bold'])
 
+    # skip last six seconds and first ten seconds
     skip_end_sec = 6
     skip_start_sec = 10
     play_duration = str(
         int(math.floor(duration) - skip_end_sec - skip_start_sec))
-    print('play duration: {}'.format(play_duration))
+    cprint(f'play duration: {play_duration} s', 'green', attrs=['bold'])
 
     rosbag_play_process = subprocess.Popen([
         'rosbag', 'play', rosbag_path, '-r', '1.0', '--clock', '-u',
@@ -111,8 +158,10 @@ def main():
     while not rospy.is_shutdown():
         # check if the python process is still running
         if rosbag_play_process.poll() is not None:
-            print('rosbag process has stopped')
-            print('Data was saved in :: ', save_data_path)
+            cprint('rosbag process has stopped', 'green', attrs=['bold'])
+            pickle.dump(data, open(path_to_pkl, 'wb'))
+            cprint(
+                f'Data was saved to :: {path_to_pkl}', 'green', attrs=['bold'])
             exit(0)
 
     rospy.spin()
